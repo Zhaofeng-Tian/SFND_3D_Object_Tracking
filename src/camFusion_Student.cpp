fa1e580a0117e38c4e5b5c8de85ea3e6a06f2a35
jinchaolu <30065376+jinchaolu@users.noreply.github.com>
2020-04-26 15:51:51 -0400

    feat: Complete FP.2
    Complete FP.2 : Compute Lidar-based TTC

--- src/camFusion_Student.cpp
+++ src/camFusion_Student.cpp
@@ -153,6 +153,29 @@ void computeTTCLidar(std::vector<LidarPoint> &lidarPointsPrev,
                      std::vector<LidarPoint> &lidarPointsCurr, double frameRate, double &TTC)
 {
     // ...
+    // auxiliary variables
+    double dT = 0.1;        // time between two measurements in seconds
+    double laneWidth = 4.0; // assumed width of the ego lane
+
+    // find closest distance to Lidar points within ego lane
+    double minXPrev = 1e9, minXCurr = 1e9;
+    for (auto it = lidarPointsPrev.begin(); it != lidarPointsPrev.end(); ++it)
+    {
+      if (abs(it->y) <= laneWidth/2.0) {
+        minXPrev = minXPrev > it->x ? it->x : minXPrev;
+      }
+      
+    }
+
+    for (auto it = lidarPointsCurr.begin(); it != lidarPointsCurr.end(); ++it)
+    {
+      if (abs(it->y) <= laneWidth/2.0) {
+        minXCurr = minXCurr > it->x ? it->x : minXCurr;
+      }
+    }
+
+    // compute TTC from both measurements
+    TTC = minXCurr * dT / (minXPrev - minXCurr);
 }
 
 
